51a82914cb0f7cd38203ec731a0f6aa932fa2dad
Gilpop8663 <wolfye@naver.com>
2023-11-03 11:58:14 +0900

    11660

--- BOJ/11660/index.js
+++ BOJ/11660/index.js
@@ -1,11 +1,67 @@
-// const input = require('fs').readFileSync(0, 'utf-8').trim();
+const input = require('fs').readFileSync(0, 'utf-8').trim();
 
 function solution(input) {
   const [info, ...data] = input.split('\n').map((item) => item.trim());
 
-  return answer;
+  const [N, M] = info.split(' ').map((item) => +item);
+
+  const arrTmp = data
+    .slice(0, N)
+    .map((item) => item.split(' ').map((item) => +item));
+
+  const tmp = data
+    .slice(-M)
+    .map((item) => item.split(' ').map((item) => +item));
+
+  const arr = Array.from({ length: N + 1 }, () =>
+    Array.from({ length: N + 1 }, () => 0)
+  );
+
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= N; j++) {
+      arr[i][j] = arrTmp[i - 1][j - 1];
+    }
+  }
+
+  const dp = Array.from({ length: N + 2 }, () =>
+    Array.from({ length: N + 2 }, () => 0)
+  );
+
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= N; j++) {
+      // console.log(dp[i + 1][j]);
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + arr[i][j] - dp[i - 1][j - 1];
+      // dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1] - dp[i][j] + arr[i][j];
+    }
+  }
+
+  const answer = [];
+
+  tmp.forEach((item) => {
+    const [stX, stY, enX, enY] = item;
+
+    const num =
+      dp[enX][enY] - dp[stX - 1][enY] - dp[enX][stY - 1] + dp[stX - 1][stY - 1];
+
+    answer.push(num);
+
+    // console.log(num);
+  });
+
+  let str = '';
+  for (let i = 1; i <= N; i++) {
+    for (let j = 1; j <= N; j++) {
+      str += dp[i][j] + ' ';
+    }
+    str += '\n';
+  }
+  // console.log(str);
+  // console.log(arr);
+  // console.log(tmp);
+
+  return answer.join('\n');
 }
 
-// const output = console.log(solution(input));
+const output = console.log(solution(input));
 
-module.exports = solution;
+// module.exports = solution;
--- BOJ/11660/index.test.js
+++ BOJ/11660/index.test.js
@@ -2,11 +2,18 @@ const solution = require('.');
 
 describe('예제', () => {
   test('예제1', () => {
-    const input = `8 2
-        2 2
-  5 5`;
+    const input = `4 3
+    1 2 3 4
+    2 3 4 5
+    3 4 5 6
+    4 5 6 7
+    2 2 3 4
+    3 4 3 4
+    1 1 4 4`;
 
-    const answer = `6`
+    const answer = `27
+    6
+    64`
       .split('\n')
       .map((item) => item.trim())
       .join('\n');
@@ -15,11 +22,18 @@ describe('예제', () => {
   });
 
   test('예제2', () => {
-    const input = `3 2
-    1 1
-    3 3`;
+    const input = `2 4
+    1 2
+    3 4
+    1 1 1 1
+    1 2 1 2
+    2 1 2 1
+    2 2 2 2`;
 
-    const answer = `2`
+    const answer = `1
+    2
+    3
+    4`
       .split('\n')
       .map((item) => item.trim())
       .join('\n');
